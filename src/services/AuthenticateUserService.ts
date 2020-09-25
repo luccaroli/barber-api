22d8f0cdd379143dffe73fae4696a0b5860f645c
luccaroli <luccaroli20@gmail.com>
2020-09-25 03:07:32 -0300

    refactor:authenticated routes

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 
+import authConfig from '../config/auth'
 import User from '../models/User'
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorret email/password combination.')
     }
 
-    const token = sign({}, '785a46a118845fe0839303085387e313', {
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d'
+      expiresIn,
     })
 
     return {
